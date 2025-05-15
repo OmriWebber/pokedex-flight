efd30f90bab6c6e16d69c5dfc7678dc32891a320
Omri <omriwebber@gmail.com>
2025-05-15 17:43:41 +1200

    feat: add generateMetadata function for dynamic Pokemon page

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -4,11 +4,68 @@ import getPokemon from '@/utils/getPokemon';
 import { normalizePokemon } from '@/utils/normalizePokemon';
 import Head from 'next/head';
 import capitaliseFirstLetter from '@/utils/capatilise';
+import { Metadata } from 'next';
 
 interface PokemonPageProps {
   params: Promise<{ 
     name: string 
   }>;
+}
+
+export async function generateMetadata({ params }: PokemonPageProps) {
+  const { name } = await params;
+
+  const { pokemonData, pokemonSpeciesData } = await getPokemon({ name });
+
+  if (!(pokemonData && pokemonSpeciesData)) {
+    return {
+      title: 'Pokedex | Flight',
+      description: 'No data found',
+    };
+  }
+
+  const evolution = await fetcher(pokemonSpeciesData.evolution_chain.url);
+
+  // Normalize the data
+  const pokemon = normalizePokemon({
+    pokemon: pokemonData,
+    species: pokemonSpeciesData,
+    evolution,
+  });
+
+  // Capitalize the first letter of each word in the name
+  const formatedName = pokemon.name
+    .toLowerCase()
+    .split(' ')
+    .map((word: string) => capitaliseFirstLetter(word))
+    .join(' ');
+
+  // Return metadata for the page
+  return {
+    title: formatedName + ` | Pokedex - Flight`,
+    description: `Stats for ${formatedName}`,
+    openGraph: {
+      title: formatedName,
+      description: `Stats for ${formatedName}`,
+      images: [
+        {
+          url: pokemon.image,
+          alt: formatedName,
+        },
+      ],
+    },
+    twitter: {
+      title: formatedName,
+      description: `Stats for ${formatedName}`,
+      images: [
+        {
+          url: pokemon.image,
+          alt: formatedName,
+        },
+      ],
+      card: 'summary_large_image',
+    },
+  };
 }
 
 export default async function Pokemon({ params }: PokemonPageProps) {
@@ -31,6 +88,7 @@ export default async function Pokemon({ params }: PokemonPageProps) {
 
   const evolution = await fetcher(pokemonSpeciesData.evolution_chain.url);
 
+  // Normalize the data
   const pokemon = normalizePokemon({
     pokemon: pokemonData,
     species: pokemonSpeciesData,
@@ -46,17 +104,12 @@ export default async function Pokemon({ params }: PokemonPageProps) {
 
   return (
     <>
-      <Head>
-        <title>{formatedName}</title>
-        <meta name="description" content={`Stats for ${formatedName}`} />
-        <meta property="og:title" content={formatedName} />
-      </Head>
-
       <PokemonDetailsCard pokemon={pokemon} />
     </>
   );
 }
 
+// Generates static paths for the dynamic route
 export async function generateStaticParams() {
   const pokemons = await fetcher('https://pokeapi.co/api/v2/pokemon?limit=10');
   return pokemons.results.map((pokemon: any) => ({
